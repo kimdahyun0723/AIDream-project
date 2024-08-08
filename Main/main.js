791647e21837a5ffe4c46f0d4cb4324194590060
dahyun <enqnsl64@naver.com>
2024-08-08 15:43:02 +0900

    전체 수정

--- Main/main.js
+++ Main/main.js
@@ -1,17 +1,18 @@
-    const headerMenuItems = document.querySelectorAll('#headermenu > li');
+// 헤더 동작
+const headerMenuItems = document.querySelectorAll('#headermenu > li');
 
-    headerMenuItems.forEach(item => {
-      item.addEventListener('mouseenter', () => {
-        const allSubmenus = document.querySelectorAll('.submenu');
-        allSubmenus.forEach(submenu => {
+headerMenuItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    const allSubmenus = document.querySelectorAll('.submenu');
+    allSubmenus.forEach(submenu => {
           submenu.style.display = 'block';
-        });
-      });
-
-      item.addEventListener('mouseleave', () => {
-        const allSubmenus = document.querySelectorAll('.submenu');
-        allSubmenus.forEach(submenu => {
-          submenu.style.display = 'none';
-        });
-      });
     });
+  });
+
+  item.addEventListener('mouseleave', () => {
+    const allSubmenus = document.querySelectorAll('.submenu');
+    allSubmenus.forEach(submenu => {
+      submenu.style.display = 'none';
+    });
+  });
+});
