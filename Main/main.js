ab3f6314294f3278ddcb15d51c015d0310b4b886
junseon <kimkva@naver.com>
2024-08-15 14:05:21 +0900

    form 수정

--- Main/main.js
+++ Main/main.js
@@ -1,18 +1,30 @@
 // 헤더 동작
-const headerMenuItems = document.querySelectorAll('#headermenu > li');
+const headerMenuItems = document.querySelectorAll("#headermenu > li");
 
-headerMenuItems.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    const allSubmenus = document.querySelectorAll('.submenu');
-    allSubmenus.forEach(submenu => {
-          submenu.style.display = 'block';
+headerMenuItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "block";
     });
   });
 
-  item.addEventListener('mouseleave', () => {
-    const allSubmenus = document.querySelectorAll('.submenu');
-    allSubmenus.forEach(submenu => {
-      submenu.style.display = 'none';
+  item.addEventListener("mouseleave", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "none";
     });
+  });
+});
+
+// submenu의 li를 클릭해을때 li 안에 a 태그가 작동
+const submenuItems = document.querySelectorAll(".submenu li");
+submenuItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    const link = item.querySelector("a");
+    if (link) {
+      window.location.href = link.href; // 링크로 이동
+    }
+    event.stopPropagation(); // 상위 li의 클릭 이벤트 방지
   });
 });
