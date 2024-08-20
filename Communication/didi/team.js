a9e84bbcce9e3b361462bde1204037e6a6f2aede
junseon <kimkva@naver.com>
2024-08-20 18:23:24 +0900

    친구에게 물어봐

--- Communication/didi/team.js
+++ Communication/didi/team.js
@@ -1,101 +1,37 @@
-const rowsPerPage = 10;
-const rows = document.querySelectorAll("#my-table tbody tr");
-const rowsCount = rows.length; // 100/8  12.9 -> 13
-const pageCount = Math.ceil(rowsCount / rowsPerPage);
-const numbers = document.querySelector("#numbers");
-//변수 추가
-const prevPageBtn = document.querySelector(".pagination .fa-arrow-left");
-const nextPageBtn = document.querySelector(".pagination .fa-arrow-right");
-let pageActiveIdx = 0; //현재 보고 있는 페이지그룹 번호
-let currentPageNum = 0; //현재 보고 있는 페이지네이션 번호
-let maxPageNum = 3; // 페이지그룹 최대 개수
+// 헤더 동작
+const headerMenuItems = document.querySelectorAll("#headermenu > li");
 
-for (let i = 1; i <= pageCount; i++) {
-  numbers.innerHTML += `<li><a href="">${i}</a></li>`;
-}
+headerMenuItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "block";
+    });
+  });
 
-const numberBtn = numbers.querySelectorAll("a");
-console.log(numberBtn);
-
-//페이지네이션 번호 감추기
-for (nb of numberBtn) {
-  nb.style.display = "none";
-}
-
-numberBtn.forEach((item, idx) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    //테이블 출력 함수
-    displayRow(idx);
+  item.addEventListener("mouseleave", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "none";
+    });
   });
 });
 
-function displayRow(idx) {
-  let start = idx * rowsPerPage;
-  let end = start + rowsPerPage;
-
-  let rowsArray = [...rows];
-
-  for (ra of rowsArray) {
-    ra.style.display = "none";
-  }
-
-  let newRows = rowsArray.slice(start, end);
-  for (nr of newRows) {
-    nr.style.display = "";
-  }
-  for (nb of numberBtn) {
-    nb.classList.remove("active");
-  }
-  numberBtn[idx].classList.add("active");
-} //displayRow
-
-displayRow(0);
-
-//페이지네이션 그룹 표시 함수
-function displayPage(num) {
-  //페이지네이션 번호 감추기
-  for (nb of numberBtn) {
-    nb.style.display = "none";
-  }
-  let totalPageCount = Math.ceil(pageCount / maxPageNum);
-
-  let pageArr = [...numberBtn];
-  let start = num * maxPageNum;
-  let end = start + maxPageNum;
-  let pageListArr = pageArr.slice(start, end);
-  console.log(num, start, end, pageArr, pageListArr);
-
-  for (let item of pageListArr) {
-    item.style.display = "block";
-  }
-  if (pageActiveIdx == 0) {
-    prevPageBtn.style.display = "none";
-  } else {
-    prevPageBtn.style.display = "block";
-  }
-  if (pageActiveIdx == totalPageCount - 1) {
-    nextPageBtn.style.display = "none";
-  } else {
-    nextPageBtn.style.display = "block";
-  }
-}
-displayPage(0);
-
-nextPageBtn.addEventListener("click", () => {
-  let nextPageNum = pageActiveIdx * maxPageNum + maxPageNum;
-  displayRow(nextPageNum);
-  ++pageActiveIdx;
-  displayPage(pageActiveIdx);
+// submenu의 li를 클릭해을때 li 안에 a 태그가 작동
+const submenuItems = document.querySelectorAll(".submenu li");
+submenuItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    const link = item.querySelector("a");
+    if (link) {
+      window.location.href = link.href; // 링크로 이동
+    }
+    event.stopPropagation(); // 상위 li의 클릭 이벤트 방지
+  });
 });
-prevPageBtn.addEventListener("click", () => {
-  let nextPageNum = pageActiveIdx * maxPageNum - maxPageNum;
-  displayRow(nextPageNum);
-  --pageActiveIdx;
-  displayPage(pageActiveIdx);
-});
+
 // 모집 상태 필터링 기능 추가
 const filterButtons = document.querySelectorAll(".filter-button");
+const rows = document.querySelectorAll("#my-table tbody tr");
 
 filterButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
--- Communication/team1/team.js
+++ Communication/team1/team.js
@@ -1,5 +1,37 @@
+// 헤더 동작
+const headerMenuItems = document.querySelectorAll("#headermenu > li");
+
+headerMenuItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "block";
+    });
+  });
+
+  item.addEventListener("mouseleave", () => {
+    const allSubmenus = document.querySelectorAll(".submenu");
+    allSubmenus.forEach((submenu) => {
+      submenu.style.display = "none";
+    });
+  });
+});
+
+// submenu의 li를 클릭해을때 li 안에 a 태그가 작동
+const submenuItems = document.querySelectorAll(".submenu li");
+submenuItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    const link = item.querySelector("a");
+    if (link) {
+      window.location.href = link.href; // 링크로 이동
+    }
+    event.stopPropagation(); // 상위 li의 클릭 이벤트 방지
+  });
+});
+
 // 모집 상태 필터링 기능 추가
 const filterButtons = document.querySelectorAll(".filter-button");
+const rows = document.querySelectorAll("#my-table tbody tr");
 
 filterButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
